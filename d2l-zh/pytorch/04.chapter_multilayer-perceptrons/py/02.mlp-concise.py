18e760376f6638b0d4f352299d51e8ba98ad2554
wp133716 <wongp1998@gmail.com>
2022-02-28 21:30:49 +0800

    chapter4增加3个py文件

---
+++ d2l-zh/pytorch/04.chapter_multilayer-perceptrons/py/02.mlp-concise.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+
+if __name__ == "__main__":
+    net = nn.Sequential(nn.Flatten(),
+                        nn.Linear(784, 256),
+                        nn.ReLU(),
+                        nn.Linear(256, 10))
+
+    def init_weights(m):
+        if type(m) == nn.Linear:
+            nn.init.normal_(m.weight, std=0.01)
+
+    net.apply(init_weights)
+
+    batch_size, lr, num_epochs = 256, 0.5, 100
+    loss = nn.CrossEntropyLoss()
+    trainer = torch.optim.SGD(net.parameters(), lr=lr)
+
+    train_iter, test_iter = d2l.load_data_fashion_mnist(batch_size, root="/home/user/my_python_test/d2l/d2l-zh/pytorch/data/")
+    d2l.train_ch3(net, train_iter, test_iter, loss, num_epochs, trainer)
